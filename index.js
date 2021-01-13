bba130f59ad7cda263d5edfd3b0d349c05e3b259
juan_carlos_meza <juancmezat@gmail.com>
2021-01-13 11:48:33 -0800

    done but tests not running yet

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+let drivers = ['Milo', 'Otis', 'Garfield']
+
+let arrayFunctions = []
+arrayFunctions.push(drivers)
+
+function destructivelyAppendDriver(name){
+    drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+}
+
+function 'destructivelyRemoveLastDriver(){
+    drivers.pop()
+}
+
+function 'destructivelyRemoveFirstDriver(){
+    drivers.shift()
+}
+
+function appendDriver(name){
+    let newDrivers = drivers.slice().push(name)
+    return newDrivers
+}
+
+function prependDriver(name){
+    let newDrivers = drivers.slice().unshift(name)
+    return newDrivers
+}
+
+function removeLastDriver(){
+    let newDrivers = drivers.slice().pop()
+    return newDrivers
+}
+
+function removeFirstDriver(){
+    let newDrivers = drivers.slice().shift()
+    return newDrivers
+}
